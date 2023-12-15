bbbf3ff58afc2a231e9348d017b1a6407a8e13ad
ss263 <ss263@ford.com>
2023-12-16 00:17:39 +0530

    project_structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import '../src/Components/dashboard'
 
+import { BrowserRouter, Routes, Route, Switch } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SideMenu from './Components/sidemenu';
+import LandingPage from '../src/Components/dashboard';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-     Shobana
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LandingPage/>
     </div>
   );
 }
